fc5b22db8c54d38c7e7fe93e3c5745b05440bcd3
EduardoBrazSilva <eduardobrazsilva45@gmail.com>
2024-07-09 14:12:21 -0300

    True Create route create trips

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,9 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-Trips";
 
 const app = fastify()
 
-app.get('/cadastrar', async () => {
-    await prisma.trip.create({
-        data: {
-            destintion: 'Florianopolis',
-            starts_at: new(Date),
-            ends_at: new(Date),
-
-        }
-    })
-
-    return 'Registro cadastrado com sucesso!'
-})
-
-app.get('/listar', () => {
-    return 'Hello NLW'
-})
-
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("Server Running!")
