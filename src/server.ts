90e8fc02239ac33b8f838d8c5ccb10d2bb6b1445
EduardoBrazSilva <eduardobrazsilva45@gmail.com>
2024-07-09 10:57:55 -0300

    Config fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/test', () => {
+    return 'Hello NLW'
+})
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("Server Running!")
+})
