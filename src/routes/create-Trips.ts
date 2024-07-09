ee81f08bac10f1109b6be6389497aae576fc611f
EduardoBrazSilva <eduardobrazsilva45@gmail.com>
2024-07-09 14:37:55 -0300

    config zod routes schema

--- src/routes/create-Trips.ts
+++ src/routes/create-Trips.ts
@@ -1,7 +1,23 @@
 import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod"
 
 export async function createTrip(app: FastifyInstance) {
-    app.post('/trips', async () => {
-        return "Hello world!"
+    app.withTypeProvider<ZodTypeProvider>().post('/trips',{ 
+      schema: {
+        body: z.object({
+            destination: z.string().min(4),
+            stats_at: z.coerce.date(),
+            ends_at: z.coerce.date(),
+        }) 
+      },
+    }, async (request) => {
+        const { destination, ends_at, stats_at } = request.body
+        
+        return {
+            destination,
+            ends_at,
+            stats_at
+        }
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import fastify from "fastify";
 import { createTrip } from "./routes/create-Trips";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 
