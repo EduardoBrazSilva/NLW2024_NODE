e8bd2abe0648c0edd0dd80ded0e1265b2f510dbc
Eduardo Braz <eduardobrazsilva45@gmail.com>
2024-07-14 18:15:16 -0300

    finish confirmation trip email

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod"
 import { prisma } from "../lib/prisma";
+import nodemailer from "nodemailer";
 import { getMailClient } from "../lib/mail";
 import { dayjs } from "../lib/dayjs";
 
@@ -18,6 +19,13 @@ export async function confirmTrip(app: FastifyInstance) {
         const trip = await prisma.trip.findUnique({
             where: {
                 id: tripId,
+            },
+            include: {
+                participants: {
+                    where: {
+                        is_owner: false,
+                    }
+                }
             }
         })
 
@@ -37,10 +45,39 @@ export async function confirmTrip(app: FastifyInstance) {
         const formatedStartDate = dayjs(trip.starts_at).format('LL');
         const formatedEndDate = dayjs(trip.ends_at).format('LL');
 
-        const confirmationLink = `http:/localhost:3333/trips/${trip.id}/confirm`
-
         const mail = await getMailClient()
 
-        return { tripId: request.params.tripId}
+        await Promise.all(
+            trip.participants.map(async (participant) => {
+                const confirmationLink = `http:/localhost:3333/trips/${trip.id}/confirm/${participant.id}`
+
+                const message = await mail.sendMail({
+                    from: {
+                      name: 'Equipe plann.er',
+                      address: 'oi@plann.er'
+                    },
+                    to: participant.email,
+                    subject: `Confirme sua presença na viagem para ${trip.destination} em ${formatedStartDate}`,
+                    html: `
+                      <div style = "font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+                        <p>Você foi convidado para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formatedStartDate} até ${formatedEndDate}</strong>.</p>
+                        <p></p>
+                        <p>Para confirmar sua presença na viagem, clique no link abaixo: </p>
+                        <p></p>
+                        <p>
+                          <a href= "${confirmationLink}"> Confirmar viagem</a>
+                        </p>
+                        <p></p>
+                        <p>Caso você não saiba do que se trata esse email, apenas o ignore</p>
+                      </div>
+                    `.trim()
+                  })
+          
+                  console.log(nodemailer.getTestMessageUrl(message))                
+            })
+        )
+
+
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`)
     })
 }
